6c2120d3b4bcdfc12ac902370f16f025144a38ab
Pierre Basck <pierre.basck@gmail.com>
2022-09-21 20:07:38 +0200

    hello

--- simple-calculator/script.js
+++ simple-calculator/script.js
@@ -37,7 +37,7 @@ let catImg = document.getElementById("img");
 
 function game() {
 
-    let number = prompt('Give a number bewteen 1 and 100');
+    let number = prompt('Give a number between 1 and 100');
     while (number !== random) {
 
 if (number > random) {
@@ -55,8 +55,9 @@ else if (number < random) {
 } 
 else {
         if(count === 1){console.log(`${playerName} WINS! In ${count} try!`)} else {console.log(`${playerName} WINS! In ${count} tries!`)};
-        if(count === 1){divy.innerHTML = `Congrats ${playerName}! The Number was ${random}! You got it in ${count} try!`}
-        else{divy.innerHTML = `Congrats ${playerName}! The Number was ${random}! You got it in ${count} tries!`};
+        if(count === 1){divy.innerHTML = `Congrats ${playerName}! The Number was ${random}! You got it in ${count} try! What a pro!`}
+        else if(count > 1 && count < 10){divy.innerHTML = `Congrats ${playerName}! The Number was ${random}! You got it in ${count} tries! Pretty Good!`}
+        else {divy.innerHTML = `Congrats ${playerName}! The Number was ${random}! You got it in ${count} tries! You're not really good at this, are you?!`};
         catImg.style.display = "block";
         break;
     }
@@ -64,7 +65,7 @@ else {
 
 
 let play = prompt(`${playerName}, do you want to play a game? Y/N`)
-if(play === "Y" || play === "y") {
+if(play === "Y" || play === "y" || play === "yes" || play === "yis") {
     game();
 } else {console.log("Too Bad!")}
 
